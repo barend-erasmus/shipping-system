90718ed352d2c4a0a44fd20e486b40cc06df38b8
developersworkspace@gmail.com <developersworkspace@gmail.com>
2018-07-04 19:40:23 +0200

    update sql

--- src/repositories/orders.ts
+++ src/repositories/orders.ts
@@ -1,7 +1,7 @@
 import { inject, injectable } from 'inversify';
-import * as uuid from 'uuid';
 import { Order } from '../entities/order';
 import { IWritableRepository } from '../interfaces/writable-repository';
+import { Account } from '../value-objects/account';
 import { BaseRepository } from './base';
 
 @injectable()
@@ -15,7 +15,35 @@ export class OrderRepository implements IWritableRepository<Order, string> {
     }
 
     public async find(id: string): Promise<Order> {
-        throw new Error('Method not implemented.');
+        const rows: any[] = await this.baseRepository.query(`SELECT * FROM ORDERS WHERE ID = @id`, {
+            id,
+        });
+
+        if (!rows.length) {
+            return null;
+        }
+
+        const row: any = rows[0];
+
+        return new Order(
+            row.ID,
+            new Account(
+                row.ACCOUNT_NUMBER,
+                row.ACCOUNT_EMAIL_ADDRESS,
+                row.ACCOUNT_NAME,
+            ),
+            row.APPROVED,
+            row.CANCELLED,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+            null,
+        );
     }
 
     public async findAll(): Promise<Order[]> {
@@ -23,57 +51,44 @@ export class OrderRepository implements IWritableRepository<Order, string> {
     }
 
     public async insert(entity: Order): Promise<Order> {
-        // this.baseRepository.execute(`INSERT INTO ORDERS (
-        //     ID
-        //     ACCOUNT_NUMBER,
-        //     ACCOUNT_EMAIL_ADDRESS,
-        //     ACCOUNT_NAME,
-        //     APPROVED,
-        //     CANCELLED,
-        //     COLLECTION_TIMESTAMP,
-        //     CONFIRMED,
-        //     COST,
-        //     DECLINED,
-        //     DELIVERY_TIMESTAMP,
-        //     DESTINATION_ID,
-        //     HEIGHT,
-        //     LENGTH
-        //     WIDTH,
-        //     SOURCE_ID,
-        //     WEIGHT
-        // ) VALUES (
-        //     '${uuid.v4()}',
-        //     '${entity.account.accountNumber}',
-        //     '${entity.account.emailAddress}',
-        //     '${entity.account.name}',
-        //     ${entity.approved},
-        //     '${entity.cancelled}',
-        //     '${entity.collectionTimestamp.getTime()}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-        //     '${}',
-
-        // );
-        // `);
+        await this.baseRepository.execute(`INSERT INTO ORDERS (
+            ID,
+            ACCOUNT_NUMBER,
+            ACCOUNT_EMAIL_ADDRESS,
+            ACCOUNT_NAME,
+            APPROVED,
+            CANCELLED,
+            COLLECTION_TIMESTAMP,
+            CONFIRMED,
+            COST,
+            DECLINED,
+            DELIVERY_TIMESTAMP,
+            DESTINATION_ID,
+            HEIGHT,
+            LENGTH,
+            WIDTH,
+            SOURCE_ID,
+            WEIGHT
+        ) VALUES (
+            '${entity.id}',
+            '${entity.account.accountNumber}',
+            '${entity.account.emailAddress}',
+            '${entity.account.name}',
+            ${entity.approved},
+            ${entity.cancelled},
+            ${entity.collectionTimestamp ? entity.collectionTimestamp.getTime() : null},
+            ${entity.confirmed},
+            ${entity.cost},
+            ${entity.declined},
+            ${entity.deliveryTimestamp ? entity.deliveryTimestamp.getTime() : null},
+            ${entity.destination.id},
+            ${entity.dimensions.height},
+            ${entity.dimensions.length},
+            ${entity.dimensions.width},
+            ${entity.source.id},
+            ${entity.weight}
+        );
+        `);
 
         return entity;
     }
